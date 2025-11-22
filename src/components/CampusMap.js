507d84b78c4a1949f98297f0fa19a49dbb8c1849
Grace <graceim0801@gmail.com>
2025-11-22 14:19:53 -0500

    Fix runtime error in map component - remove require() for marker icons

--- src/components/CampusMap.js
+++ src/components/CampusMap.js
@@ -4,14 +4,6 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import './CampusMap.css';
 
-// Fix for default marker icons in react-leaflet
-delete L.Icon.Default.prototype._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
-});
-
 // Custom marker icons based on visit status
 const createCustomIcon = (isVisited) => {
   return L.divIcon({
